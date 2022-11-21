a087274b069bad2f5efe9e25bf604e150cb68b83
Dimitry-prog <kfifa3@gmail.com>
2022-11-21 18:44:04 +0300

    feat: routes

---
+++ routes/CardRouter.js
@@ -0,0 +1,8 @@
+import Router from 'express';
+import CardController from '../controllers/CardController.js';
+
+const router = new Router();
+
+router.get('/cards', CardController.getAll);
+router.post('/cards', CardController.create);
+router.delete('/cards/:cardId', CardController.remove);
---
+++ routes/UserRouter.js
@@ -0,0 +1,8 @@
+import Router from 'express';
+import UserController from '../controllers/UserController.js';
+
+const router = new Router();
+
+router.get('/users', UserController.getAll);
+router.get('/users/:userId', UserController.getSingle);
+router.post('/users', UserController.create);
