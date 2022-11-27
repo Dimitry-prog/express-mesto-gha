f92c93fef2ca3ec9bf78537ac8b31e94dfecef89
Dimitry-prog <kfifa3@gmail.com>
2022-11-27 23:16:33 +0300

    fix: errors

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const DB_URL = 'mongodb://localhost:27017/mestodb';
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({
+  extended: true,
+}));
 app.use((req, res, next) => {
   console.log(req);
   req.user = {
